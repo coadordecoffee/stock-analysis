6d3537af762b413711ed55e88a3962336992abc8
Matheus N <matheusnaschaves@gmail.com>
2022-10-17 23:35:11 +0200

    stock analysis

---
+++ src/aggregators/Aggregator.java
@@ -0,0 +1,26 @@
+package aggregators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Aggregator {
+	
+	List<Double> numbers;
+	
+	public Aggregator(){
+		numbers = new ArrayList<Double>();
+	}
+	
+	public Aggregator(List<Double> numbers){
+		numbers = new ArrayList<Double>(numbers);
+	}
+	 
+	public void add(double number) {
+		numbers.add(number);
+	} 
+	
+	public abstract double calculate();
+	
+	public abstract List<Double> getValues();
+	
+}
